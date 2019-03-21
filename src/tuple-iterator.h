ee1e3e34e2eaa778b422b2b9a5a50cf2b0a81553
Brian Rodriguez <brian@brianrodri.com>
2019-03-21 06:07:19 -0400

    s/tuple_iterator/tuple-iterator

---
+++ src/tuple-iterator.h
@@ -0,0 +1,267 @@
+#ifndef BRIANRODRI_TUPLE_ITERATOR_TUPLE_ITERATOR_H
+#define BRIANRODRI_TUPLE_ITERATOR_TUPLE_ITERATOR_H
+#include <functional>
+#include <iostream>
+#include <optional>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <variant>
+
+namespace tuple_ext {
+namespace detail {
+
+// Helper aliases to help cut down boilerplate when working with compile-time
+// index values.
+template <size_t I>
+using index_t = std::integral_constant<size_t, I>;
+
+template <size_t I>
+constexpr auto index_c = index_t<I>{};
+
+// Exposes types required by TupleIterator to be standard-compliant.
+//
+// The types are derived from the given type parameter, which is assumed to be a
+// "tuple-like" structure. Specifically, `Tup` must compile in the following
+// contexts:
+//   - std::get<I>(std::declval<Tup&>())
+//   - std::tuple_size<Tup>
+//
+// std::tuple defines these overloads thanks to the standard, but you can create
+// overloads for custom classes as necessary.
+template <typename Tup>
+struct IterTraitsImpl;
+
+template <typename... T>
+struct IterTraitsImpl<std::tuple<T...>> {
+  private:
+    // Returns a variant of compile-time index values from the provided sequence
+    // of index values.
+    template <size_t... I> static constexpr auto
+    MakeIndexVariant(std::index_sequence<I...>) -> std::variant<index_t<I>...> {
+        // NOTE: This function is only inspected at compile-time — never called.
+        return {};
+    };
+
+  public:
+    using PointerType = std::variant<T*...>;
+    using ReferenceType = std::variant<std::reference_wrapper<T>...>;
+    using ValueType = ReferenceType;
+    using DifferenceType = std::ptrdiff_t;
+
+    // An implementation detail for remembering the current index which a tuple
+    // iterator is pointing to.
+    using IndexVariant =
+        decltype(MakeIndexVariant(std::index_sequence_for<T...>()));
+};
+
+}  // namespace detail
+
+// Provides interface for creating tuple iterators.
+template <typename T>
+class TupleRange;
+
+template <typename T>
+class TupleIterator {
+    using IndexVariantOpt =
+        std::optional<typename detail::IterTraitsImpl<T>::IndexVariant>;
+
+  public:
+    // Type aliases expected by the standard.
+    using value_type = typename detail::IterTraitsImpl<T>::ValueType;
+    using reference = typename detail::IterTraitsImpl<T>::ReferenceType;
+    using pointer = typename detail::IterTraitsImpl<T>::PointerType;
+    using difference_type = typename detail::IterTraitsImpl<T>::DifferenceType;
+
+    // TODO: Investigate making this iterator random-access.
+    using iterator_category = std::bidirectional_iterator_tag;
+
+    // Returns a *singular iterator*, that is, an iterator that is not
+    // associated with any tuple. Such instances are semantically equivalent to
+    // nullptr, and should therefore never be modified or dereferenced.
+    //
+    // You can check if an instance is singular by comparing it against nullptr.
+    explicit TupleIterator(std::nullptr_t _ = {})
+        : tuple_ptr_{nullptr}, index_opt_{std::nullopt} {}
+
+    TupleIterator& operator=(std::nullptr_t _) {
+        tuple_ptr_ = nullptr;
+        index_opt_ = std::nullopt;
+    }
+
+    TupleIterator(const TupleIterator<T>& src) = default;
+    TupleIterator(TupleIterator<T>&& src) = default;
+    TupleIterator& operator=(const TupleIterator<T>& src) = default;
+    TupleIterator& operator=(TupleIterator<T>&& src) = default;
+
+    constexpr TupleIterator& operator++() {
+        Increment();
+        return *this;
+    }
+
+    constexpr TupleIterator operator++(int _) {
+        TupleIterator curr_iter{*this};
+        Increment();
+        return curr_iter;
+    }
+
+    constexpr TupleIterator& operator--() {
+        Decrement();
+        return *this;
+    }
+
+    constexpr TupleIterator operator--(int _) {
+        TupleIterator curr_iter{*this};
+        Decrement();
+        return curr_iter;
+    }
+
+    // NOTE: operator-> is not defined because there is no way to make it
+    // standard-compliant.
+    //
+    // The standard expects that values returned by operator-> may eventually be
+    // resolved by 1+ repeated applications. However, we can only return a
+    // std::variant of pointers. This implies that eventually, a call to
+    // std::visit *must be made*.
+    //
+    // For now, I've chosen to simply leave out the definition of operator->
+    // while keeping the definition of a "pointer"-type. I kept the pointer-type
+    // because it is required by std::distance.
+
+    constexpr reference operator*() {
+        return std::visit([this](auto i) -> reference {
+            return {std::ref(std::get<i>(*tuple_ptr_))};
+        }, *index_opt_);
+    }
+
+    constexpr reference operator*() const {
+        return std::visit([this](auto i) -> reference {
+            return {std::cref(std::get<i>(*tuple_ptr_))};
+        }, *index_opt_);
+    }
+
+    // Returns the index of the element currently being pointed to by the
+    // iterator, or the size of the tuple if the iterator points to it's end.
+    constexpr size_t index() const {
+        return IsEnd() ? std::tuple_size_v<T> : index_opt_->index();
+    }
+
+  private:
+    // This constructor will be called by the TupleRange class methods.
+    constexpr TupleIterator(T& t, IndexVariantOpt i = {})
+        : tuple_ptr_{&t}, index_opt_{i} {};
+
+    // Provides interface for creating tuple iterators.
+    friend class TupleRange<T>;
+
+    // Provides interface for comparing tuple iterators.
+    template <typename U, typename V>
+    friend constexpr bool operator==(const TupleIterator<U>&,
+                                     const TupleIterator<V>&);
+    template <typename U>
+    friend constexpr bool operator==(const TupleIterator<U>&, std::nullptr_t);
+    template <typename U>
+    friend constexpr bool operator==(std::nullptr_t, const TupleIterator<U>&);
+
+    constexpr void Increment() {
+        if (!IsEnd()) {
+            index_opt_ = std::visit([](auto i) -> IndexVariantOpt {
+                if constexpr (i + 1 < std::tuple_size_v<T>) {
+                    return {detail::index_c<i + 1>};
+                } else {
+                    return {};
+                }
+            }, *index_opt_);
+        }
+    }
+
+    constexpr void Decrement() {
+        if (IsEnd()) {
+            // Can iterate backwards from end() when target tuple is non-empty.
+            if constexpr (0 < std::tuple_size_v<T>) {
+                index_opt_ = detail::index_c<std::tuple_size_v<T> - 1>;
+            }
+        } else {
+            index_opt_ = std::visit([](auto i) -> IndexVariantOpt {
+                if constexpr (i > 0) {
+                    return {detail::index_c<i - 1>};
+                } else {
+                    return {detail::index_c<0>};
+                }
+            }, *index_opt_);
+        }
+    }
+
+    constexpr bool IsEnd() const { return index_opt_ == std::nullopt; }
+
+    T* tuple_ptr_;
+    IndexVariantOpt index_opt_;
+};
+
+template <typename T, typename U>
+constexpr bool operator==(const TupleIterator<T>& lhs,
+                          const TupleIterator<U>& rhs) {
+    if constexpr (std::is_same_v<T, U>) {
+        return lhs.tuple_ptr_ == rhs.tuple_ptr_ &&
+               lhs.index_opt_ == rhs.index_opt_;
+    } else {
+        return false;
+    }
+}
+
+template <typename T>
+constexpr bool operator==(const TupleIterator<T>& lhs, std::nullptr_t _) {
+    return lhs.tuple_ptr_ == nullptr;
+}
+
+template <typename T>
+constexpr bool operator==(std::nullptr_t _, const TupleIterator<T>& rhs) {
+    return rhs.tuple_ptr_ == nullptr;
+}
+
+template <typename T, typename U>
+constexpr bool operator!=(const TupleIterator<T>& lhs,
+                          const TupleIterator<U>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T>
+constexpr bool operator!=(const TupleIterator<T>& lhs, std::nullptr_t rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T>
+constexpr bool operator!=(std::nullptr_t lhs, const TupleIterator<T>& rhs) {
+    return !(lhs == rhs);
+}
+
+// Provides interface for creating tuple iterators.
+template <typename T>
+class TupleRange {
+  public:
+    constexpr TupleRange(T& t) : tuple_ptr_(&t) {}
+
+    constexpr TupleIterator<T> begin() const {
+        if constexpr (0 < std::tuple_size_v<T>) {
+            return {*tuple_ptr_, detail::index_c<0>};
+        } else {
+            return end();
+        }
+    }
+
+    constexpr TupleIterator<T> end() const { return {*tuple_ptr_}; }
+
+    static constexpr TupleIterator<T> begin(T& t) {
+        return TupleRange{t}.begin();
+    }
+
+    static constexpr TupleIterator<T> end(T& t) {
+        return TupleRange{t}.end();
+    }
+
+  private:
+    T* tuple_ptr_;
+};
+
+}  // namespace tuple_ext
+#endif  // BRIANRODRI_TUPLE_ITERATOR_TUPLE_ITERATOR_H
---
+++ test/tuple-iterator_test.cpp
@@ -0,0 +1,117 @@
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include "gtest/gtest.h"
+#include "tuple-iterator.h"
+
+class TupleIteratorTest : public ::testing::Test {
+  protected:
+    using Tuple = std::tuple<int, std::vector<double>, std::string>;
+    using TupleIterator = tuple_ext::TupleIterator<Tuple>;
+    using TupleRange = tuple_ext::TupleRange<Tuple>;
+
+    Tuple tuple_{1, {1.61803, 2.71828, 3.14159}, "inf"};
+    TupleRange tuple_range_{tuple_};
+};
+
+TEST_F(TupleIteratorTest, CopyConstructibleConceptSatisfied) {
+    const TupleIterator tuple_begin = tuple_range_.begin();
+
+    // { T a = b; } -> { a == b; }
+    TupleIterator tuple_begin_copy = tuple_begin;
+    ASSERT_EQ(tuple_begin_copy, tuple_begin);
+
+    // { T(a) == a; }
+    EXPECT_EQ(TupleIterator{tuple_begin}, tuple_begin);
+}
+
+TEST_F(TupleIteratorTest, CopyAssignableConceptSatisfied) {
+    const TupleIterator tuple_begin = tuple_range_.begin();
+
+    TupleIterator tuple_iter = tuple_range_.end();
+    ASSERT_NE(tuple_iter, tuple_begin);
+
+    // { a = b; } -> { a == b }
+    tuple_iter = tuple_begin;
+    EXPECT_EQ(tuple_iter, tuple_begin);
+}
+
+TEST_F(TupleIteratorTest, SwappableConceptSatisfied) {
+    ASSERT_NE(tuple_range_.begin(), tuple_range_.end());
+    TupleIterator iter_a = tuple_range_.begin();
+    TupleIterator iter_b = tuple_range_.end();
+
+    using std::swap;
+    swap(iter_a, iter_b);
+
+    ASSERT_NE(iter_a, iter_b);
+    EXPECT_EQ(iter_a, tuple_range_.end());
+    EXPECT_EQ(iter_b, tuple_range_.begin());
+}
+
+TEST_F(TupleIteratorTest, IteratorConceptSatisfied) {
+    TupleIterator iter = tuple_range_.begin();
+
+    using reference = typename std::iterator_traits<TupleIterator>::reference;
+    EXPECT_TRUE((std::is_same_v<decltype(*iter), reference>));
+    EXPECT_TRUE((std::is_same_v<decltype(++iter), TupleIterator&>));
+}
+
+TEST_F(TupleIteratorTest, DefaultConstructibleConceptSatisfied) {
+    TupleIterator default_initialized_iter;
+    EXPECT_EQ(default_initialized_iter, nullptr);
+
+    TupleIterator value_initialized_iter{};
+    EXPECT_EQ(value_initialized_iter, nullptr);
+
+    EXPECT_EQ(TupleIterator(), nullptr);
+    EXPECT_EQ(TupleIterator{}, nullptr);
+}
+
+TEST_F(TupleIteratorTest, CopyEqualityConceptSatisfied) {
+    TupleIterator a = tuple_range_.begin();
+    TupleIterator b = tuple_range_.begin();
+
+    ASSERT_EQ(a, b);
+    EXPECT_EQ(++a, ++b);
+}
+
+TEST_F(TupleIteratorTest, ForwardIteratorConceptSatisfied) {
+    TupleIterator i = tuple_range_.begin();
+    std::vector<TupleIterator> i_copies(3, i);
+
+    EXPECT_EQ(++i, std::next(tuple_range_.begin()));
+    for (TupleIterator& j : i_copies) { EXPECT_EQ(j, tuple_range_.begin()); }
+
+    i = tuple_range_.begin();
+    EXPECT_EQ(i++, tuple_range_.begin());
+    EXPECT_EQ(i, std::next(tuple_range_.begin()));
+
+    /* TODO: Add dereference tests.
+    i = tuple_range_.begin();
+    EXPECT_EQ(*i++, *tuple_range_.begin());
+    EXPECT_EQ(*i, *std::next(tuple_range_.begin()));
+    */
+}
+
+TEST_F(TupleIteratorTest, BidirectionalIteratorConceptSatisfied) {
+    TupleIterator i = tuple_range_.end();
+    std::vector<TupleIterator> i_copies(3, i);
+
+    --i;
+    for (TupleIterator& j : i_copies) { EXPECT_EQ(j, tuple_range_.end()); }
+
+    TupleIterator j = i = tuple_range_.end();
+    auto equivalent_expression = [&i]() {
+        TupleIterator temp = i;
+        --i;
+        return temp;
+    };
+    EXPECT_EQ(equivalent_expression(), j--);
+
+    /* TODO: Add dereference tests.
+    i = tuple_range_.end();
+    EXPECT_EQ(*i--, *tuple_range_.end());
+    EXPECT_EQ(*i, *std::next(tuple_range_.end()));
+    */
+}
