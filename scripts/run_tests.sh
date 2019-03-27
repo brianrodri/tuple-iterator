5d24a1a8df3fabb7cf2a23ca062f3dfdae4a5072
Brian Rodriguez <brian@brianrodri.com>
2019-03-27 00:23:52 -0400

    s/unit_test/unit-test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ $1 ] && [ $1 = '--clean' ]; then rm -rf build; fi
 mkdir -p build
-cmake -B build . && make -C build && ./build/bin/unit_tests
+cmake -B build . && make -C build && ./build/bin/unit-tests
